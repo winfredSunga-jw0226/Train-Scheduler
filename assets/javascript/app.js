c3b11e88546bdd1cd3beae75c67209e23b03b4ec
Winfred Sunga <winfredsunga01@gmail.com>
2017-10-05 17:22:44 -0700

    added logic to display 0 minutes away whenever the frequency minutes equal the minutes away value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,7 @@
   var firstArrivalTime = "";
   var frequencyMinutes = 0; 
 
-  //variables which be derived
+  //variables which be derived using Moment.js
   var nextArrival = "";
   var minutesAway = 0;
 
@@ -33,26 +33,24 @@
     frequencyMinutes = sv.frequencyMinutes;
     firstArrivalTime = moment({hour : sv.firstArrivalTime.split(":")[0], minute : sv.firstArrivalTime.split(":")[1]}); 
 
-    console.log("first arrival time : " + firstArrivalTime);
+    // console.log("first arrival time : " + firstArrivalTime);
 
-    var secondsElapsedFromFirstArrival = moment().diff(firstArrivalTime, "seconds");
+    //get the total # of minutes elapsed from the start time - this will help us derived the minutes away value
+    var minutesElapsedFromFirstArrival = moment().diff(firstArrivalTime, "minutes");
 
-    console.log("secondsElapsedFromFirstArrival : " + secondsElapsedFromFirstArrival);
+    // console.log("minutesElapsedFromFirstArrival : " + minutesElapsedFromFirstArrival);
 
-    var secondsToNextArrival = parseInt(frequencyMinutes * 60) - parseInt(secondsElapsedFromFirstArrival) % parseInt(frequencyMinutes * 60);
-    var minutesAway = Math.round(secondsToNextArrival / 60); //using Math.round so that we can show 0 minutes to next arrival
-    
-    console.log("minutesToNextArrival : " + secondsToNextArrival / 60);
+    //derive the minutes away value
+    var minutesAway = parseInt(frequencyMinutes) - parseInt(minutesElapsedFromFirstArrival) % parseInt(frequencyMinutes);
 
-    nextArrival = moment().add(Math.ceil(secondsToNextArrival / 60), 'm').format("hh:mm A");
+    //I want to see "0 minutes away" from the schedule whenever the current time matches the next arrival time
+    if (minutesAway === parseInt(frequencyMinutes)) {
+      minutesAway = 0;
+    } 
+
+    //derive the next arrival time
+    nextArrival = moment().add(minutesAway, 'm').format("hh:mm A");
   
-
-
-    // console.log(trainName);
-    // console.log(destination);
-    // console.log(firstArrivalTime);
-    // console.log(frequencyMinutes);
-
     //create a new table row
     var row = $("<tr>");
 
@@ -61,7 +59,7 @@
     var td2 = $("<td>").addClass("td2").text(destination);
     var td3 = $("<td>").addClass("td3").text(frequencyMinutes);
     var td4 = $("<td>").addClass("td4").text(nextArrival);
-    var td5 = $("<td>").addClass("td5").text(minutesAway);
+    var td5 = $("<td>").addClass("td5").text(minutesAway); //(minutesAway);
 
     row.append(td1, td2, td3, td4, td5);
 
